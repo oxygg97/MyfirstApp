f40695d864b2f2be3f1b4af8450fbbb10294d479
ala ben cheikh <cheikhoxy@gmail.com>
2021-08-30 11:21:27 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+          <h1 className="title red">Your name here</h1>
+          <br />
+          <img src="imageInSrc.jpg" alt="" />
+          <br />
+          <img src="imageInPublic.jpg" alt="" />
+        </div>
+        <video width={320} height={240} controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </div>
+  );
+};
+
+export default App;
